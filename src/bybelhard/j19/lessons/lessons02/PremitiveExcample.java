9dcf3ecc4c0383a854934e84d9bd69620cbc5084
Artem <energiex@mail.ru>
2019-12-10 20:46:44 +0300

    energiex@mail.ru

---
+++ src/bybelhard/j19/lessons/lessons02/PremitiveExcample.java
@@ -0,0 +1,19 @@
+package bybelhard.j19.lessons.lessons02;
+
+//bybelhard.j19.lessons.lessons02
+
+public class PremitiveExcample {
+
+
+
+    public static void main(String[] args) {
+        short sh1 = 5;
+        int i = 100;
+        double d = 3.14;
+
+        System.out.println(sh1+1/d);
+
+
+        }}
+
+
