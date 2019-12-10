ddc0cbffc0220367f4e0d41578acddaf9e69444a
Artem <energiex@mail.ru>
2019-12-10 21:29:41 +0300

    2

--- src/bybelhard/j19/lessons/lessons02/PremitiveExcample.java
+++ src/bybelhard/j19/lessons/lessons02/PremitiveExcample.java
@@ -4,14 +4,21 @@ package bybelhard.j19.lessons.lessons02;
 
 public class PremitiveExcample {
 
-
+//psvm
 
     public static void main(String[] args) {
-        short sh1 = 5;
+      /*  short sh1 = 5; //
         int i = 100;
         double d = 3.14;
 
         System.out.println(sh1+1/d);
+*/
+
+int i=0;
+        System.out.println(i++);
+        System.out.println(++i);
+
+
 
 
         }}
